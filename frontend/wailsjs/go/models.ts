10266bd8781194545d336c2d7d8dc3160e68e32d
flipfloppy1 <flipfloppy.business@gmail.com>
2025-03-23 02:37:55 +1030

    Added wails model

---
+++ frontend/wailsjs/go/models.ts
@@ -0,0 +1,13 @@
+export namespace main {
+	
+	export enum Screen {
+	    SEARCH = "Search",
+	    CREATURES = "Creatures",
+	    OBJECTS = "Objects",
+	    LIQUIDS = "Liquids",
+	    LORE = "Lore",
+	    MECHANICS = "Mechanics",
+	}
+
+}
+
